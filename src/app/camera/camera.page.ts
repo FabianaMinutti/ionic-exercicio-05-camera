645574b40150211983f092c35a4afe5a0242fc5f
pos liberado <pospos@labcomp.usc>
2019-09-14 10:22:22 -0300

    Add camera

---
+++ src/app/camera/camera.page.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
+
+@Component({
+  selector: 'app-camera',
+  templateUrl: './camera.page.html',
+  styleUrls: ['./camera.page.scss'],
+})
+export class CameraPage implements OnInit {
+
+  constructor(private camera: Camera) { }
+
+  ngOnInit() {    
+  }
+
+  public takePicture() {
+    const options: CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.FILE_URI,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE
+    }
+
+    this.camera.getPicture(options).then((imageData) => {
+      let base64Image = 'data:image/jpeg;base64,' + imageData;
+     }, (err) => {
+     });
+  }
+}
